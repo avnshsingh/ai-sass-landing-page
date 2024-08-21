08cf98b25c8bb389db76c6489c49a5645f030b0d
Avinash Singh <avinashsingh888555@gmail.com>
2024-08-21 19:34:52 +0530

    done with the header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,14 +1,39 @@
+import LogoIcon from "@/assets/logo.svg";
+import MenuIcon from "@/assets/icon-menu.svg";
+
 export const Header = () => {
   return (
-    <header>
+    <header className="py-4 border-b border-white/15 md:border-none">
       <div className="container">
-        <div>
+        <div className="flex items-center justify-between md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl m-auto">
           <div>
-            <span>logo2</span>
+            <div className="border size-10 inline-flex justify-center items-center border-white/15">
+              <LogoIcon className="size-8" />
+            </div>
           </div>
-          <div>
-            <button>Join Waitlist</button>
-            <span>Menu</span>
+
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-sm">
+              <a href="#" className="text-white/70 hover:text-white transition">
+                Features
+              </a>
+              <a href="#" className="text-white/70 hover:text-white transition">
+                Developers
+              </a>
+              <a href="#" className="text-white/70 hover:text-white transition">
+                Pricing
+              </a>
+              <a href="#" className="text-white/70 hover:text-white transition">
+                Changelog
+              </a>
+            </nav>
+          </div>
+
+          <div className="flex gap-4 items-center">
+            <button className="border py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a]">
+              Join Waitlist
+            </button>
+            <MenuIcon className="size-8 md:hidden" />
           </div>
         </div>
       </div>
